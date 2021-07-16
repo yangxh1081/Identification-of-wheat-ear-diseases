e13d18b50da69343c11e5645a3d35885486da630
yangxh1081 <87253982+yangxh1081@users.noreply.github.com>
2021-07-16 15:11:52 +0800

    Add files via upload

---
+++ model/model_base.py
@@ -0,0 +1,158 @@
+'''
+Refers to https://github.com/kobiso/CBAM-keras and https://github.com/xiaochus/MobileNetV3
+'''
+
+
+
+from keras.layers import Conv2D, DepthwiseConv2D, Dense, GlobalAveragePooling2D
+from keras.layers import Activation, BatchNormalization, Add, Multiply, Reshape
+from keras import backend as K
+from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, Reshape, Dense, multiply, Permute, Concatenate, Conv2D, Add, Activation, Lambda
+from keras import backend as K
+from keras.activations import sigmoid
+
+
+
+def channel_attention(input_feature, ratio=8):
+
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    channel = input_feature._keras_shape[channel_axis]
+
+    shared_layer_one = Dense(channel//ratio,
+                             activation='relu',
+                             kernel_initializer='he_normal',
+                             use_bias=True,
+                             bias_initializer='zeros')
+    shared_layer_two = Dense(channel,
+                             kernel_initializer='he_normal',
+                             use_bias=True,
+                             bias_initializer='zeros')
+
+    avg_pool = GlobalAveragePooling2D()(input_feature)
+    avg_pool = Reshape((1,1,channel))(avg_pool)
+    assert avg_pool._keras_shape[1:] == (1,1,channel)
+    avg_pool = shared_layer_one(avg_pool)
+    assert avg_pool._keras_shape[1:] == (1,1,channel//ratio)
+    avg_pool = shared_layer_two(avg_pool)
+    assert avg_pool._keras_shape[1:] == (1,1,channel)
+
+    max_pool = GlobalMaxPooling2D()(input_feature)
+    max_pool = Reshape((1,1,channel))(max_pool)
+    assert max_pool._keras_shape[1:] == (1,1,channel)
+    max_pool = shared_layer_one(max_pool)
+    assert max_pool._keras_shape[1:] == (1,1,channel//ratio)
+    max_pool = shared_layer_two(max_pool)
+    assert max_pool._keras_shape[1:] == (1,1,channel)
+
+    cbam_feature = Add()([avg_pool,max_pool])
+    cbam_feature = Activation('sigmoid')(cbam_feature)
+
+    if K.image_data_format() == "channels_first":
+        cbam_feature = Permute((3, 1, 2))(cbam_feature)
+
+    return multiply([input_feature, cbam_feature])
+
+def spatial_attention(input_feature):
+    kernel_size = 7
+
+    if K.image_data_format() == "channels_first":
+        channel = input_feature._keras_shape[1]
+        cbam_feature = Permute((2,3,1))(input_feature)
+    else:
+        channel = input_feature._keras_shape[-1]
+        cbam_feature = input_feature
+
+    avg_pool = Lambda(lambda x: K.mean(x, axis=3, keepdims=True))(cbam_feature)
+    assert avg_pool._keras_shape[-1] == 1
+    max_pool = Lambda(lambda x: K.max(x, axis=3, keepdims=True))(cbam_feature)
+    assert max_pool._keras_shape[-1] == 1
+    concat = Concatenate(axis=3)([avg_pool, max_pool])
+    assert concat._keras_shape[-1] == 2
+    cbam_feature = Conv2D(filters = 1,
+                    kernel_size=kernel_size,
+                    strides=1,
+                    padding='same',
+                    activation='sigmoid',
+                    kernel_initializer='he_normal',
+                    use_bias=False)(concat)
+    assert cbam_feature._keras_shape[-1] == 1
+
+    if K.image_data_format() == "channels_first":
+        cbam_feature = Permute((3, 1, 2))(cbam_feature)
+
+    return multiply([input_feature, cbam_feature])
+
+class ModelBase:
+    def __init__(self, shape, n_class, alpha=1.0):
+        self.shape = shape
+        self.n_class = n_class
+        self.alpha = alpha
+
+    def _relu6(self, x):
+        """Relu 6
+        """
+        return K.relu(x, max_value=6.0)
+
+    def _hard_swish(self, x):
+        """Hard swish
+        """
+        return x * K.relu(x + 3.0, max_value=6.0) / 6.0
+
+    def _return_activation(self, x, nl):
+        if nl == 'HS':
+            x = Activation(self._hard_swish)(x)
+        if nl == 'RE6':
+            x = Activation(self._relu6)(x)
+        if nl == 'RE':
+            x = Activation("relu")(x)
+
+        return x
+
+    def _conv_block(self, inputs, filters, kernel, strides, nl):
+        channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
+
+        x = Conv2D(filters, kernel, padding='same', strides=strides)(inputs)
+        x = BatchNormalization(axis=channel_axis)(x)
+
+        return self._return_activation(x, nl)
+
+
+    def _cbam(self, inputs):
+        x = channel_attention(inputs, 8)
+        x = spatial_attention(x)
+        return x
+
+    def _bottleneck(self, inputs, filters, kernel, e, s, cbam, nl):
+
+        channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
+        input_shape = K.int_shape(inputs)
+
+        tchannel = int(e)
+        cchannel = int(self.alpha * filters)
+
+        r = s == 1 and input_shape[3] == filters
+
+        x = self._conv_block(inputs, tchannel, (1, 1), (1, 1), nl)
+
+        x = DepthwiseConv2D(kernel, strides=(s, s), depth_multiplier=1, padding='same')(x)
+
+        x = BatchNormalization(axis=channel_axis)(x)
+        x = self._return_activation(x, nl)
+
+        if cbam:
+            x = self._cbam(x)
+
+        x = Conv2D(cchannel, (1, 1), strides=(1, 1), padding='same')(x)
+        x = BatchNormalization(axis=channel_axis)(x)
+
+        if r:
+            x = Add()([x, inputs])
+
+        return x
+
+
+    def build(self):
+        pass
+
+
+
